2d5bda3124deb33bc70eed4c561a257b1c210743
Sarah Downard <sarah@Sarahs-2014-MacBook-Pro.local>
2021-09-19 14:43:49 +1000

    Pre push commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("testing");
+
+const homeButton = document.querySelector("#home-button");
+console.log (homeButton);
+
+homeButton.addEventListener('onclick', homePage);
+
+// function homePage()
+//   location.href 
+
