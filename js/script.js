2127d06d04b0eed457ad9b59b0563e041d5aaf4c
Sarah Downard <sarah@Sarahs-2014-MacBook-Pro.local>
2021-09-20 17:02:03 +1000

    More changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
 console.log("testing");
 
-const homeButton = document.querySelector("#home-button");
-console.log (homeButton);
 
-homeButton.addEventListener('onclick', homePage);
+const touringNavButton = document.getElementById('touring-nav-button');
 
-// function homePage()
-//   location.href 
+touringNavButton.addEventListener('click', followLink);
+
+function followLink(){
+  sideMenu.classList.add('show-menu');
+}
+
+
+
+
+
+// const gridImage = document.querySelector("#grid-image")
+// console.log(gridImage);
+
+// gridImage.addEventListener("mouseover", enlargeImage);
+
+// function enlargeImage() {
+// 	// gridImage.style.height="120%";
+// 	// gridImage.style.width="120%";
+// 	// gridImage.style.position='relative';
+// 	gridImage.style.zIndex='1';
+//  }
+
+// gridImage.addEventListener("mouseleave", resetImage);
+
+// function resetImage() {
+// 	gridImage.style.height="100%";
+// 	gridImage.style.width="100%";
+// 	gridImage.style.position='relative';
+//  }
 
