b9a6bab5bbe3dd2acc84f54eaffbe6c880284eb2
Sarah Downard <sarah@Sarahs-2014-MacBook-Pro.local>
2021-09-25 22:52:55 +1000

    Many Changes

--- js/script.js
+++ js/script.js
@@ -1,38 +1,37 @@
 console.log("testing");
 
-  
-  
-    // <!-- Trigger toggleText() when the 
-    //     button is clicked -->
-
-
-function toggleText() {
-  
 var points = document.getElementById("points");
-console.log(points);
-console.log(showMoreText);
-console.log(buttonText);
-
+    
 var showMoreText = document.getElementById("moreText");
-  
+
 var buttonText = document.getElementById("textButton");
-  
+
+
+buttonText.addEventListener("onclick", toggleText);
+
+function toggleText(blogId, spanClass) {
+  console.log(document.getElementsByClassName(spanClass)[0]);
+  console.log(document.getElementById(blogId));
+  console.log(blogId, spanClass);
+
+var showMoreText = document.getElementsByClassName(spanClass)[0];
+var buttonText = document.getElementById(blogId);
 
 if (points.style.display === "none") {
-  
+
   showMoreText.style.display = "none";
-  
+    
   points.style.display = "inline";
-  
+    
   buttonText.innerHTML = "Show More";
-            }  
+  }
+    
 else {
-  
-showMoreText.style.display = "inline";
-  
-points.style.display = "none";
-  
-buttonText.innerHTML = "Show Less";
+
+  showMoreText.style.display = "inline";
+    
+  points.style.display = "none";
+    
+  buttonText.innerHTML = "Show Less";
   }
 }
-
