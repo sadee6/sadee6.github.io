a08b737356247e3d6fa2a55ce154f2f8d1358f70
Sarah Downard <sarah@Sarahs-2014-MacBook-Pro.local>
2021-09-28 06:40:30 +1000

    Photo fix and ssmall changes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 
 
 function toggleText(blogId) {
+// console.log(blogId);
 
+// find elements within the closest .travel-locations container
 var parentContainer = event.target.closest(".travel-locations");
 
+// assign variables from within the parent container
 var points = parentContainer.querySelector("#points");
 
 var showMoreText = parentContainer.querySelector("#moreText");
 
 var buttonText = parentContainer.querySelector("#textButton");
 
+// when button clicked within the parent container then do below...
 buttonText.addEventListener("onclick", toggleText);
 
 if (points.style.display === "none") {
